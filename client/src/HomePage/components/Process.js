e117517326a9e0f1dba92f9d8b1ae35d3eea53b1
Samsul Ratul <samsulratul98@gmail.com>
2020-04-23 22:52:56 +0600

    Footer Added

---
+++ client/src/HomePage/components/Process.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Card}from 'react-bootstrap'
+import './Process.css'
+
+const Process = () => {
+    return <div class="container-fluid process">
+        <div class="row">
+            <div class="col-xl-6 ml-5">
+            <p className="h1 font-weight-normal ml-3" style={{color: '#010f54'}}>Process</p>
+            <p className="h1 ml-3 mb-3" style={{color: '#010f54'}}>How it work</p>
+            <div class="row">
+                <div class="col-xl-2 text-center justify-content-center mt-4">
+                   <button class="btn btn-lg text-center rounded-circle" style={{backgroundColor: '#E58C8A'}}>01</button>
+                </div>
+                <div class="col-xl-10 mb-5 ml-n3">
+                    <Card className="border-0" style={{backgroundColor: '#F4F8FB'}}>
+                        <Card.Body>
+                            <Card.Title>
+                                <p className="h4 font-weight-bold">Download App or Visit Website</p>
+                            </Card.Title>
+                            <Card.Text>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ised in the 1960s with the release of Letraset sheets containing Lorem
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div class="col-xl-2 text-center justify-content-center mt-4">
+                   <button class="btn btn-lg text-center rounded-circle" style={{backgroundColor: '#E58C8A'}}>02</button>
+                </div>
+                <div class="col-xl-10 mb-5 ml-n3">
+                    <Card className="border-0" style={{backgroundColor: '#F4F8FB'}}>
+                        <Card.Body>
+                            <Card.Title>
+                                <p className="h4 font-weight-bold">Login with valid ID</p>
+                            </Card.Title>
+                            <Card.Text>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ised in the 1960s with the release of Letraset sheets containing Lorem
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div class="col-xl-2 text-center justify-content-center mt-4">
+                   <button class="btn btn-lg text-center rounded-circle" style={{backgroundColor: '#E58C8A'}}>03</button>
+                </div>
+                <div class="col-xl-10 mb-5 ml-n3">
+                    <Card className="border-0" style={{backgroundColor: '#F4F8FB'}}>
+                        <Card.Body>
+                            <Card.Title>
+                                <p className="h4 font-weight-bold">Create A Patient</p>
+                            </Card.Title>
+                            <Card.Text>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ised in the 1960s with the release of Letraset sheets containing Lorem
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div class="col-xl-2 text-center justify-content-center mt-4">
+                   <button class="btn btn-lg text-center rounded-circle" style={{backgroundColor: '#E58C8A'}}>04</button>
+                </div>
+                <div class="col-xl-10 mb-5 ml-n3">
+                    <Card className="border-0" style={{backgroundColor: '#F4F8FB'}}>
+                        <Card.Body>
+                            <Card.Title>
+                                <p className="h4 font-weight-bold">Let Us Take Care Of Your Illness</p>
+                            </Card.Title>
+                            <Card.Text>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ised in the 1960s with the release of Letraset sheets containing Lorem
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>;
+}
+
+export default Process;
---
+++ client/src/shared/component/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+    return <footer className="page-footer p-1 mt-5" style={{backgroundColor: 'rgba(5, 6, 70, 1)'}}>
+        <p className="text-light pt-2 pl-5">Copyright &copy; All right reserved</p>
+    </footer>;
+};
+
+export default Footer;
