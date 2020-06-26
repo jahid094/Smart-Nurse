73358a38107a702e3d09258fa34db6dd6b68eccd
SirRatul <samsulratul98@gmail.com>
2020-06-26 14:34:44 +0600

    Notiification Route Added

--- routes/routines.js
+++ routes/routines.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();;
-// const moment = require('moment')
+const moment = require('moment')
+// const Moment = require('moment');
 const Routine = require('../models/Routine');
+/* const MomentRange = require('moment-range');
+const moment = MomentRange.extendMoment(Moment); */
 // const { forwardAuthenticated } = require('../config/auth')
 // const User = require('../models/User');
 
@@ -135,5 +138,82 @@ router.delete('/routine/:id'  , async(req , res) =>{
     }
 })
 
+router.post('/routineNotification' , async (req, res) =>{
+    const {id} = req.body
+    let userId
+    if(req.user){
+        userId = req.user._id
+    } else {
+        userId = id
+    }
+
+    try {
+        const routine = await Routine.find({owner: userId})
+
+        var today = new Date();
+        today = today.getFullYear() + '/' + String(today.getMonth() + 1).padStart(2, '0') + '/' + String(today.getDate()).padStart(2, '0');
+        // console.log("Today:"+moment(new Date()).format())
+
+        var compareDate;
+        var startDate;
+        var endDate;
+        var compareDateTime = moment(new Date()).format();
+        var dateTimeBetween;
+        var dateTimeRange;
+        var subtractTime;
+        var notificationArray = [];
+
+        routine.forEach(function(entry) {
+            /* console.log("Start Loop")
+            console.log(entry.startDate);
+            console.log(entry.endDate); */
+            compareDate = moment(today, "YYYY/MM/DD");
+            startDate   = moment(entry.startDate, "YYYY/MM/DD");
+            endDate     = moment(entry.endDate, "YYYY/MM/DD");
+            subtractTime = entry.notification.split(' ')
+            // console.log('Subtract Minute:'+subtractTime[1])
+            for(let time of entry.times){
+                // console.log("Start Time Loop")
+                var endDateTime = moment(`${today} ${time.time}`, 'YYYY-MM-DD HH:mm').format()
+                var startDateTime = moment(endDateTime).subtract(subtractTime[1], 'minutes').format();
+                /* console.log('Compare DateTime: '+compareDateTime);
+                console.log('Start DateTime: '+startDateTime);
+                console.log('End DateTime: '+endDateTime); */
+                dateTimeBetween = moment(compareDateTime, 'YYYY-MM-DD HH:mm').isBetween(moment(startDateTime, 'YYYY-MM-DD HH:mm'), moment(endDateTime, 'YYYY-MM-DD HH:mm'), null, '[]')
+                if(dateTimeBetween || moment(compareDateTime, 'YYYY-MM-DD HH:mm').isSame(startDateTime, 'YYYY-MM-DD HH:mm') || moment(compareDateTime, 'YYYY-MM-DD HH:mm').isSame(endDateTime, 'YYYY-MM-DD HH:mm')){
+                    // console.log('If')
+                    dateTimeRange = true
+                } else{
+                    // console.log('Else')
+                    dateTimeRange = false
+                }
+                /* console.log('DateTimeRange: '+dateTimeRange)
+                console.log("End Time Loop") */
+                if(dateTimeRange){
+                    // console.log('Date Time Range Block')
+                    notificationArray.push(entry)
+                    break;
+                } else {
+                }
+            }
+            // console.log("End Loop")
+        });
+
+
+        if (notificationArray === undefined || notificationArray.length == 0) {
+            return res.status(404).json({
+                message: "You have no upcoming notification"
+            })
+        }
+
+        return res.json({
+            routine: notificationArray
+        })
+    } catch (e) {
+        res.status(500).json({
+           message: e
+        })
+    }
+})
 
 module.exports = router
